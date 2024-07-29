52e35cf4d6a96aa93ae86312edffb398c62dcf0a
Steven <yenjung178741@gmail.com>
2024-07-29 16:12:05 +1000

    complete form test totally finished Register.test.jsx

--- front-end/src/__tests__/sqlite3_read_script.py
+++ front-end/src/__tests__/sqlite3_read_script.py
@@ -1,6 +1,10 @@
 import sqlite3
+import os
 
-sql_connection = sqlite3.connect('../../../../InvoiceProcessing/db.sqlite3')
+direct = os.path.dirname(__file__)
+db_path = os.path.join(direct, '../../../InvoiceProcessing/db.sqlite3')
+
+sql_connection = sqlite3.connect(db_path)
 exec_cursor = sql_connection.cursor()
 
 exec_cursor.execute('delete from invoice_user where username="test-user";')
