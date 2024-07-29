4ffb004d14866b0103aebb5d89ccbb1493dec211
Steven <yenjung178741@gmail.com>
2024-07-29 17:25:30 +1000

    modify some changes

--- front-end/src/__tests__/sqlite3_read_script.py
+++ front-end/src/__tests__/sqlite3_read_script.py
@@ -8,10 +8,6 @@ sql_connection = sqlite3.connect(db_path)
 exec_cursor = sql_connection.cursor()
 
 exec_cursor.execute('delete from invoice_user where username="test-user";')
-# with open('clear_login_test.sql', 'r') as f:
-#     sql_script = f.read()
-
-# exec_cursor.executescript(sql_script)
 
 sql_connection.commit()
 sql_connection.close()
