aacc1ea731ace6917fa83aecda8c08e4b7b9dbb5
Steven <yenjung178741@gmail.com>
2024-07-30 15:18:48 +1000

    complete Register.jsx testing

--- front-end/src/__tests__/Register.test.jsx
+++ front-end/src/__tests__/Register.test.jsx
@@ -1,61 +1,147 @@
 import React from 'react';
 import { render, screen, fireEvent, waitFor, act } from '@testing-library/react';
-import { describe, test, expect, beforeAll, afterAll } from 'vitest';
-import { createMemoryRouter, MemoryRouter } from 'react-router-dom';
+import { describe, test, expect, beforeAll, afterAll, vi } from 'vitest';
+import { useNavigate, MemoryRouter, Route, Routes } from 'react-router-dom';
 import Register from '../views/Register';
+import Login from '../views/Login';
 import { exec } from 'child_process';
-import axios from 'axios';
+// import { routes } from "../AppRouter"
+// import axios from 'axios';
+// import { type } from 'os';
+// import { watch } from 'less';
 
 const render_register = () => {
   return render(
-    <MemoryRouter>
+    <MemoryRouter initialEntries={['/register']}>
         <Register />
     </MemoryRouter>
   );
-}
+};
 
-test('Register page element test', () => {
-  render_register();
+describe('Register unit test', () => {
+    test('Register page element test', () => {
+      render_register();
+    
+      // check h1 text
+      expect(screen.getByRole('heading', {name: "Create an account"})).toBeInTheDocument();
+    
+      // check username input field
+      const username_parent = screen.getByTestId("Register-Username");
+      const username_field = username_parent.querySelector('#Register-Username');
+      expect(username_field).toBeInTheDocument();
+    
+      // check email input field
+      const email_parent = screen.getByTestId("Register-Email");
+      const email_field = email_parent.querySelector('#Register-Email');
+      expect(email_field).toBeInTheDocument();
+    
+      // check name input field
+      const name_parent = screen.getByTestId("Register-Name");
+      const name_field = name_parent.querySelector('#Register-Name');
+      expect(name_field).toBeInTheDocument();
+    
+      // check password input field
+      const password_parent_P = screen.getByTestId("Register-password");
+      const password_field_P = password_parent_P.querySelector('#Register-password');
+      expect(password_field_P).toBeInTheDocument();
+    
+      // check confirm password input field
+      const password_parent_CP = screen.getByTestId("Register-confirm-password");
+      const password_field_CP = password_parent_CP.querySelector('#Register-confirm-password');
+      expect(password_field_CP).toBeInTheDocument();
+    
+      // check button 
+      // toggle ...
+      expect(screen.getAllByRole('button', {name: "toggle password visibility"})[0]).toBeInTheDocument();
+      // Sign-up
+      expect(screen.getByTestId('Sign-up-btn')).toBeInTheDocument();
+    
+      // check link
+      // navigate to Login page
+      expect(screen.getByRole('link', {name: "Have an account? Go Login"})).toBeInTheDocument();
+      // information
+      expect(screen.getByRole('link', {name: "By clicking Login, you agree to our Terms of Service and Privacy Policy"})).toBeInTheDocument();
+    });
 
-  // check h1 text
-  expect(screen.getByRole('heading', {name: "Create an account"})).toBeInTheDocument();
+    test('toggle password visibility button function', () => {
+      render_register();
 
-  // check username input field
-  const username_parent = screen.getByTestId("Register-Username");
-  const username_field = username_parent.querySelector('#Register-Username');
-  expect(username_field).toBeInTheDocument();
+      const password_parent_P = screen.getByTestId("Register-password");
+      const input_field = password_parent_P.querySelector("#Register-password");
+      const visibility_btn = screen.getAllByRole('button', {name: "toggle password visibility"})[0];
 
-  // check email input field
-  const email_parent = screen.getByTestId("Register-Email");
-  const email_field = email_parent.querySelector('#Register-Email');
-  expect(email_field).toBeInTheDocument();
+      // default type of input field is password
+      expect(input_field).toHaveAttribute('type', 'password');
+      
+      const enter_password = 'abc123./]'
+      
+      fireEvent.change(input_field, { target: { value: enter_password } });
+      fireEvent.click(visibility_btn);
+      
+      // after clicking the visibility button, the type should turn to text
+      expect(input_field).toHaveAttribute('type', 'text');
+      expect(input_field).toHaveAttribute('value', enter_password);
+      
+      // check if click the v btn twice, the type turns back to password
+      fireEvent.click(visibility_btn);
+      expect(input_field).toHaveAttribute('type', 'password');
+    });
 
-  // check name input field
-  const name_parent = screen.getByTestId("Register-Name");
-  const name_field = name_parent.querySelector('#Register-Name');
-  expect(name_field).toBeInTheDocument();
+    test('toggle confirm password visibility button function', () => {
+      // same as the above, but on confirm password
+      render_register();
 
-  // check password input field
-  const password_parent_P = screen.getByTestId("Register-password");
-  const password_field_P = password_parent_P.querySelector('#Register-password');
-  expect(password_field_P).toBeInTheDocument();
+      const password_parent_CP = screen.getByTestId("Register-confirm-password");
+      const input_field = password_parent_CP.querySelector("#Register-confirm-password");
+      // the second visibility_btn [1]
+      const visibility_btn = screen.getAllByRole('button', {name: "toggle password visibility"})[1];
 
-  // check confirm password input field
-  const password_parent_CP = screen.getByTestId("Register-confirm-password");
-  const password_field_CP = password_parent_CP.querySelector('#Register-confirm-password');
-  expect(password_field_CP).toBeInTheDocument();
+      expect(input_field).toHaveAttribute('type', 'password');
+      
+      const enter_password = 'abc123./]xyz'
+      
+      fireEvent.change(input_field, { target: { value: enter_password } });
+      fireEvent.click(visibility_btn);
+      
+      expect(input_field).toHaveAttribute('type', 'text');
+      expect(input_field).toHaveAttribute('value', enter_password);
+      
+      fireEvent.click(visibility_btn);
+      expect(input_field).toHaveAttribute('type', 'password');
+    });
 
-  // check button 
-  // toggle ...
-  expect(screen.getAllByRole('button', {name: "toggle password visibility"})[0]).toBeInTheDocument();
-  // Sign-up
-  expect(screen.getByTestId('Sign-up-btn')).toBeInTheDocument();
+    test('link to login page', async () => {
+        render(
+          <MemoryRouter initialEntries={['/register']}>
+            <Routes>
+              <Route path="/register" element={<Register />} />
+              <Route path="/login" element={<Login />} />
+            </Routes>
+          </MemoryRouter>
+        );
 
-  // check link
-  // navigate to Login page
-  expect(screen.getByRole('link', {name: "Have an account? Go Login"})).toBeInTheDocument();
-  // information
-  expect(screen.getByRole('link', {name: "By clicking Login, you agree to our Terms of Service and Privacy Policy"})).toBeInTheDocument();
+        const navi_login = screen.getByText("Have an account? Go Login");
+        
+        fireEvent.click(navi_login);
+
+        expect(screen.getByRole('heading', { name: "Login" })).toBeInTheDocument();
+    });
+
+    test('show agreement modal', () => {
+        render_register();
+        const TSPP = screen.getByText("By clicking Login, you agree to our Terms of Service and Privacy Policy");
+        expect(TSPP).toBeInTheDocument();
+        fireEvent.click(TSPP);
+
+        // modal headline
+        expect(screen.getByRole('heading', { name: "Use our e-invoice service?" })).toBeInTheDocument();
+
+        // disagree btn
+        expect(screen.getByRole('button', { name: "Disagree" })).toBeInTheDocument();
+
+        // agree btn
+        expect(screen.getByRole('button', { name: "Agree" })).toBeInTheDocument();
+    });
 });
 
 
