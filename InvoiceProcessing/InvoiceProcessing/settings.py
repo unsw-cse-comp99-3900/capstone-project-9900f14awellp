322d53a530cfbd70e3ad8fdddd70a41dd56b92be
v_hmingzhao <v_hmingzhao@tencent.com>
2024-07-30 10:44:55 +0800

    register error fixed

--- InvoiceProcessing/InvoiceProcessing/settings.py
+++ InvoiceProcessing/InvoiceProcessing/settings.py
@@ -133,7 +133,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = "zh-hans"
+LANGUAGE_CODE = "en-us"
 
 TIME_ZONE = "Asia/Shanghai"
 
