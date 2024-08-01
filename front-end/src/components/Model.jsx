888bb63b38ad2eea55597ed5017d6bd65de4c4c0
Shiqi Yin <z5370300@ad.unsw.edu.au>
2024-08-01 16:55:11 +1000

    change for dashboard

--- front-end/src/components/Model.jsx
+++ front-end/src/components/Model.jsx
@@ -22,7 +22,7 @@ const style = {
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
-  width: 150,
+  width: 400,
   bgcolor: "background.paper",
   boxShadow: 24,
   p: 4,
--- front-end/src/views/Dashboard.jsx
+++ front-end/src/views/Dashboard.jsx
@@ -1,31 +1,74 @@
-import React, { useEffect, useState } from 'react';
-import { Outlet} from "react-router-dom";
+import React, { useState, useEffect, useCallback } from 'react';
+import axios from 'axios';
 import { ResponsiveAppBar } from "../components/Navbar";
-import { VirtualizedList } from "../components/List";
+import OutlinedAlerts from '../components/Alert';
+import SparklesText from '@/components/SparklesText';
+import { DashboardCard } from '@/components/CardBorder';
 
 export default function Dashboard() {
-    //* 
-    const [viewportHeight, setViewportHeight] = useState(window.innerHeight);
-    useEffect(() => {
-        const handleResize = () => {
-          setViewportHeight(window.innerHeight);
-        };
+  const token = localStorage.getItem('token');
+  const [alert, setAlert] = useState(null); // 初始状态设置为null
+  const fetchNumData = useCallback(() => {
+    axios.get('http://localhost:8000/invoice/invoice-number', {
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': `Bearer ${token}`
+        }
+    })
+    .then(response => {
+        console.log(response.data);
+        setAlert({ severity: 'success', message: 'upload successfully' });
+    })
+    .catch(error => {
+        console.log(error.message);
+        setAlert({ severity: 'error', message: error.message });
+    });
+}, [token]);
+
+useEffect(() => {
+  fetchNumData();
+}, [fetchNumData]);
     
-        window.addEventListener('resize', handleResize);
-    
-        return () => {
-          window.removeEventListener('resize', handleResize);
-        };
-      }, []);
-    
-    const listHeight = viewportHeight - 70; //AppBar 的高度70
+
     return (
         <div>
             <ResponsiveAppBar />
-            <div style={{ flexGrow: 1, overflow: 'auto' }}>
-                <VirtualizedList height={listHeight}/>
-            </div>
-            <Outlet />
+            {alert && (
+              <div style={{
+                  position: 'fixed',
+                  top: '11vh',
+                  right: 10,
+                  width: '30%',
+                  zIndex: 9999
+              }}>
+                  <OutlinedAlerts severity={alert.severity} onClose={() => setAlert(null)}>
+                      {alert.message}
+                  </OutlinedAlerts>
+              </div>
+              )}
+              <div className="container mx-auto p-7"style={{
+                  width: '100%',
+                  display: 'flex',
+                  flexDirection: 'row',
+              }}>
+                <SparklesText
+                  text="Hi, Welcome Back!"
+                  colors={{ first: "#FFD700", second: "#FF4500" }}
+                  className="my-custom-class"
+                  sparklesCount={8}
+                  style={{ textAlign: 'left', fontSize: '2rem' }} // 直接在style中设置
+                />
+              </div>
+              <div style={{
+                  margin: '10px',
+                  width: '40%',
+                  padding: '10px',
+                  
+                               
+              }}>
+              <DashboardCard></DashboardCard>
+              </div>
+              
         </div>
     );
 }
