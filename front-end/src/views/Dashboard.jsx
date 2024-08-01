113ee215667d5a7e8bb9477465f9b7315910cecb
Linxuan Lyu <lin@Lins-Macbook-Pro.local>
2024-08-02 04:46:44 +1000

    finish time log

--- front-end/src/views/Dashboard.jsx
+++ front-end/src/views/Dashboard.jsx
@@ -1,26 +1,28 @@
-import React, { useState, useEffect, useCallback } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect, useCallback } from "react";
+import axios from "axios";
 import { ResponsiveAppBar } from "../components/Navbar";
-import OutlinedAlerts from '../components/Alert';
-import SparklesText from '@/components/SparklesText';
-import { DashboardCard } from '@/components/CardBorder';
-import PieActiveArc from '@/components/Pie';
-import SimpleLineChart from '@/components/SimpleLineChart';
-import { Card, CardContent, Typography } from '@mui/material';
+import OutlinedAlerts from "../components/Alert";
+import SparklesText from "@/components/SparklesText";
+import { DashboardCard } from "@/components/CardBorder";
+import PieActiveArc from "@/components/Pie";
+import SimpleLineChart from "@/components/SimpleLineChart";
+import { Card, CardContent, Typography } from "@mui/material";
 
 export default function Dashboard() {
-  const token = localStorage.getItem('token');
+  const token = localStorage.getItem("token");
   const [alert, setAlert] = useState(null); // 初始状态设置为null
   const [total, setTotal] = useState(0);
   const [success, setSuccess] = useState(0);
   const [fail, setFail] = useState(0);
   const [unvalidated, setUnvalidated] = useState(0);
   const [totalTime, setTotalTime] = useState([]);
-  const [sentTime,setSentTime] = useState([]);
-  const xLabels = [...new Set([
-    ...sentTime.map(d => d.create_date),
-    ...totalTime.map(d => d.create_date)
-  ])].sort((a, b) => new Date(a) - new Date(b));
+  const [sentTime, setSentTime] = useState([]);
+  const xLabels = [
+    ...new Set([
+      ...sentTime.map((d) => d.create_date),
+      ...totalTime.map((d) => d.create_date),
+    ]),
+  ].sort((a, b) => new Date(a) - new Date(b));
   // 按日期填充 sendInvoiceData 和 totalInvoiceData 的计数数据
   const sendDataMap = sentTime.reduce((acc, curr) => {
     acc[curr.create_date] = curr.count;
@@ -32,21 +34,27 @@ export default function Dashboard() {
     return acc;
   }, {});
 
-  const sendInvoiceCounts = xLabels.map(date => sendDataMap[date] || 0);
-  const totalInvoiceCounts = xLabels.map(date => totalDataMap[date] || 0);
+  const sendInvoiceCounts = xLabels.map((date) => sendDataMap[date] || 0);
+  const totalInvoiceCounts = xLabels.map((date) => totalDataMap[date] || 0);
   const data = [
-    { id: 0, value: success, label: 'Validation Successful' , color: '#FED5D4'},
-    { id: 1, value: fail, label: 'Validation Failed', color: '#D5F9EF' },
-    { id: 2, value: unvalidated, label: 'Unvalidated Invoices' , color: '#F8D4BC' },
+    { id: 0, value: success, label: "Validation Successful", color: "#FED5D4" },
+    { id: 1, value: fail, label: "Validation Failed", color: "#D5F9EF" },
+    {
+      id: 2,
+      value: unvalidated,
+      label: "Unvalidated Invoices",
+      color: "#F8D4BC",
+    },
   ];
   const fetchNumData = useCallback(() => {
-    axios.get('http://localhost:8000/invoice/invoice-number', {
+    axios
+      .get("http://localhost:8000/invoice/invoice-number", {
         headers: {
-            'Accept': 'application/json',
-            'Authorization': `Bearer ${token}`
-        }
-    })
-    .then(response => {
+          Accept: "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((response) => {
         console.log(response.data);
         setTotal(response.data.total_files);
         setSuccess(response.data.successful_files);
@@ -54,88 +62,96 @@ export default function Dashboard() {
         setUnvalidated(response.data.unvalidated_files);
         setTotalTime(response.data.total_invoice_timebase);
         setSentTime(response.data.send_invoice_timebase);
-        setAlert({ severity: 'success', message: 'upload successfully' });
-    })
-    .catch(error => {
+        setAlert({ severity: "success", message: "upload successfully" });
+      })
+      .catch((error) => {
         console.log(error.message);
-        setAlert({ severity: 'error', message: error.message });
-    });
-}, [token]);
+        setAlert({ severity: "error", message: error.message });
+      });
+  }, [token]);
 
-useEffect(() => {
-  fetchNumData();
-}, [fetchNumData]);
-    
+  useEffect(() => {
+    fetchNumData();
+  }, [fetchNumData]);
 
-    return (
-        <div>
-            <ResponsiveAppBar />
-            {alert && (
-              <div style={{
-                  position: 'fixed',
-                  top: '11vh',
-                  right: 10,
-                  width: '30%',
-                  zIndex: 9999
-              }}>
-                  <OutlinedAlerts severity={alert.severity} onClose={() => setAlert(null)}>
-                      {alert.message}
-                  </OutlinedAlerts>
-              </div>
-              )}
-              <div className="container mx-auto p-7"style={{
-                  width: '100%',
-                  display: 'flex',
-                  flexDirection: 'row',
-              }}>
-                <SparklesText
-                  text="Hi, Welcome Back!"
-                  colors={{ first: "#FFD700", second: "#FF4500" }}
-                  className="my-custom-class"
-                  sparklesCount={8}
-                  style={{ textAlign: 'left', fontSize: '2rem' }} // 直接在style中设置
-                />
-              </div>
-              <div style={{
-                  margin: '10px',
-                  width: '90%',
-                  padding: '10px',             
-              }}>
-              <DashboardCard
-                total={total}
-                success={success}
-                fail={fail}
-                unvalidated={unvalidated}
-              ></DashboardCard>
-              <div style={{ display: 'flex', gap: '20px', marginTop:'30px' }}>
-                <Card style={{ width: '50%', padding: '20px' }}>
-                  <CardContent>
-                    <Typography variant="h6" gutterBottom>
-                      NUMBERS
-                    </Typography>
-                    <Typography variant="body2" color="textSecondary" gutterBottom>
-                      Total invoices and sent invoices
-                    </Typography>
-                    <SimpleLineChart 
+  return (
+    <div>
+      {alert && (
+        <div
+          style={{
+            position: "fixed",
+            top: "11vh",
+            right: 10,
+            width: "30%",
+            zIndex: 9999,
+          }}
+        >
+          <OutlinedAlerts
+            severity={alert.severity}
+            onClose={() => setAlert(null)}
+          >
+            {alert.message}
+          </OutlinedAlerts>
+        </div>
+      )}
+      <div
+        className="container mx-auto p-7"
+        style={{
+          width: "100%",
+          display: "flex",
+          flexDirection: "row",
+        }}
+      >
+        <SparklesText
+          text="Hi, Welcome Back!"
+          colors={{ first: "#FFD700", second: "#FF4500" }}
+          className="my-custom-class"
+          sparklesCount={8}
+          style={{ textAlign: "left", fontSize: "2rem" }} // 直接在style中设置
+        />
+      </div>
+      <div
+        style={{
+          margin: "10px",
+          width: "90%",
+          padding: "10px",
+        }}
+      >
+        <DashboardCard
+          total={total}
+          success={success}
+          fail={fail}
+          unvalidated={unvalidated}
+        ></DashboardCard>
+        <div style={{ display: "flex", gap: "20px", marginTop: "30px" }}>
+          <Card style={{ width: "50%", padding: "20px" }}>
+            <CardContent>
+              <Typography variant="h6" gutterBottom>
+                NUMBERS
+              </Typography>
+              <Typography variant="body2" color="textSecondary" gutterBottom>
+                Total invoices and sent invoices
+              </Typography>
+              <SimpleLineChart
                 xLabels={xLabels}
                 aLine={totalInvoiceCounts}
                 bLIne={sendInvoiceCounts}
-                />
-                  </CardContent>
-                </Card>
-                <Card style={{ width: '50%', padding: '20px' }}>
-                  <CardContent>
-                    <Typography variant="h6" gutterBottom>
-                      Current Numbers
-                    </Typography>
-                    <Typography variant="body2" color="textSecondary" gutterBottom>
-                      Hover your mouse
-                    </Typography>
-                    <PieActiveArc data={data} />
-                  </CardContent>
-                </Card>
-              </div>
-            </div>  
+              />
+            </CardContent>
+          </Card>
+          <Card style={{ width: "50%", padding: "20px" }}>
+            <CardContent>
+              <Typography variant="h6" gutterBottom>
+                Current Numbers
+              </Typography>
+              <Typography variant="body2" color="textSecondary" gutterBottom>
+                Hover your mouse
+              </Typography>
+              <PieActiveArc data={data} />
+            </CardContent>
+          </Card>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
