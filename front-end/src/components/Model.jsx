32d2ba318ae3c2e8b722e3fbcd55bd7213c39b71
v_hmingzhao <v_hmingzhao@tencent.com>
2024-08-02 18:53:21 +0800

    dashboard

--- front-end/src/components/Model.jsx
+++ front-end/src/components/Model.jsx
@@ -146,7 +146,6 @@ export default function FormDialog({ onFormSubmit }) {
         onClose={handleClose}
         PaperProps={{
           component: "form",
-          component: "form",
           onSubmit: (event) => {
             event.preventDefault();
             const formData = new FormData(event.currentTarget);
