54a47f3bc604c0b5f33f82e2eddfde55338ef195
Shiqi Yin <z5370300@ad.unsw.edu.au>
2024-08-03 20:14:27 +1000

    change for welcome page

--- front-end/src/views/Welcome.jsx
+++ front-end/src/views/Welcome.jsx
@@ -1,31 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { UserTextField } from "../components/Inputs";
+import { Navbar } from "@/components/HoverNavbar";
+import { HeroParallaxDemo } from "@/components/ui/hero-parallax";
 
 export default function Welcome() {
-  //* 路由跳转
-  const navigate = useNavigate();
-  const goLogin = () => {
-    navigate("/login");
-  };
-  const goRegister = () => {
-    navigate("/register");
-  };
   return (
     <div>
-      <h1>Welcome to the Invoice App</h1>
-      <p>
-        This is a simple invoice management app that allows you to create, edit,
-        and manage invoices.
-      </p>
-      <button onClick={goLogin}>Login</button>
-      <button onClick={goRegister}>Register</button>
-      <UserTextField
-        id="login-userName"
-        label="User Name"
-        helperText="Please enter your user name"
-        defaultValue="name"
-      />
+      <Navbar></Navbar>
+      <HeroParallaxDemo></HeroParallaxDemo>
     </div>
   );
 }
